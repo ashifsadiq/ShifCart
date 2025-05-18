7b7d33e089222ab13f891787c7a519d6be881861
Ashif <contact@ashifsadiq.in>
2025-05-18 21:08:03 +0530

    Update project configuration and dependencies for navigation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import MainNavigator from './Src/MainNavigator';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => MainNavigator);
