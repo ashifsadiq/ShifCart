4f26f94ad335873b5562967ba38fcad636a0d7b7
Ashif <contact@ashifsadiq.in>
2025-05-18 21:07:45 +0530

    Add main navigation and screens for the application

---
+++ Src/MainNavigator.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+// Screens
+import HomeScreen from './Screens/Home/HomeScreen';
+import WishListScreen from './Screens/WishList/WishListScreen';
+import CartScreen from './Screens/Cart/CartScreen';
+import ProfileScreen from './Screens/Profile/ProfileScreen';
+import SearchScreen from './Screens/Search/SearchScreen';
+import SearchResultsScreen from './Screens/Search/SearchResultsScreen';
+
+// Navigators
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function BottomTabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Wishlist') {
+            iconName = focused ? 'heart' : 'heart-outline';
+          } else if (route.name === 'Cart') {
+            iconName = focused ? 'cart' : 'cart-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'green',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Wishlist" component={WishListScreen} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function MainNavigator() {
+  return (
+    <>
+      <Text>
+        <Icon name="home" />
+      </Text>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen
+            name="BottomTabNavigator"
+            component={BottomTabNavigator}
+          />
+          <Stack.Screen name="Search" component={SearchScreen} />
+          <Stack.Screen name="SearchResults" component={SearchResultsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ Src/Screens/Auth/CreateAccout/CreateAccoutScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function CreateAccoutScreen() {
+  return (
+    <View>
+      <Text>CreateAccoutScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
